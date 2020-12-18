c5634c3224abfb04c09225bee3b978d2f41284ec
a-agashe <75656825+a-agashe@users.noreply.github.com>
2020-12-18 15:54:37 +0530

    Update Employee.java

--- gitDemo/src/com/demo/Employee.java
+++ gitDemo/src/com/demo/Employee.java
@@ -22,12 +22,7 @@ public class Employee {
 	public void setAddress(String address) {
 		this.address = address;
 	}
-	public int getSalary() {
-		return salary;
-	}
-	public void setSalary(int salary) {
-		this.salary = salary;
-	}
+
 	@Override
 	public String toString() {
 		return "Employee [name=" + name + ", address=" + address + ", salary=" + salary + "]";
